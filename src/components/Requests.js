ba7c126f41c7d2af01686302ac1f4fae3005d990
kuljitkaur27 <kuljitkaur1984@gmail.com>
2020-04-09 10:19:15 +1000

    Version 1.5

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -65,7 +65,9 @@ class Requests extends Component {
                   <td>{req.price}</td>
                   <td>{req.status}</td>
                   <td>
-                    <button type="button"><Link to={"/editRequest"}>Edit</Link></button>
+                    <button type="button"><Link to={'/newRequest/',
+                                                  {data: req}} >Edit
+                                                  </Link></button>
                     &nbsp;<button type="button" onClick={() => this._deleteRequest(this,req.id)}>Delete</button>
                   </td>
                 </tr>
--- src/components/newRequest.js
+++ src/components/newRequest.js
@@ -7,15 +7,27 @@ const SERVER_URL_products = 'http://localhost:3000/products';
 
 class newRequest extends Component {
 
-    constructor(){
-      super();
+    constructor(props){
+      super(props);
       this.state = {
         b: [],
         p: [],
         reqs: [],
         total_price: 0,
-        current_date : new Date()
+        current_date : new Date(),
+        one_p: [],
+        one_b: []
       };
+
+      this.initialState = {
+           id : '',
+           branch_id: 0,
+           product_id: 0,
+           quantity: 0,
+           price: 100,
+           status: 'OPEN',
+           created_at: ''
+         }
 
       const fetchresult = () => {
 
@@ -31,8 +43,26 @@ class newRequest extends Component {
       fetchresult();
       console.log(this.state.b,this.state.p,this.state.reqs);
 
+      this._handleChange = this._handleChange.bind(this);
+
       this._handleSave = this._handleSave.bind(this);
     }
+
+    _handleChange(event) {
+
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value
+    })
+    // this.setState({
+    //   one_p: p.filter(one_p => one_p.product_id !== this.state.product_id)
+    //   this.state.price: ${one_p.price * this.state.quantity}
+    // })
+
+    }
+
 
     // Event listener
     _handleSave(event) {
@@ -45,24 +75,36 @@ class newRequest extends Component {
 
 
   render(){
+    console.log(this.state.id);
+    let pageTitle;
+    if(this.state.id) {
+      pageTitle = <h2>Edit Request</h2>
+    } else {
+      pageTitle = <h2>Add Request</h2>
+    }
+
+
     return(
       <div>
-      <h3>New Request</h3>
-      <form>
-      <label>Branch:</label><select id="selectBranches">
+      <h3>{pageTitle}</h3>
+      <form onSubmit={this._handleSave}>
+      <label>Branch:</label><select id="selectBranches" value={this.state.branch_id}
+      onChange={this._handleChange}>
       {this.state.b.map((branch,key) => (
           <option>{branch.branch_name}</option>
       ))}
       </select> <br/>
-      <label>Product:</label><select id="selectProduct">
+      <label>Product:</label><select id="selectProduct" value={this.state.product_id}
+      onChange={this._handleChange}>
       {this.state.p.map((product,key) => (
           <option>{product.item}</option>
       ))}
       </select> <br/>
-      <label>Quantity:</label><input type="number" /><br/>
-      <label>Status:</label><input type="text" value="OPEN" readOnly="true" /><br/>
-      <label>Created on:</label><input type="date" value={this.state.current_date} /><br/>
-      <input type="submit" value="Save" onClick={ this._handleSave }/>
+      <label>Quantity:</label><input type="number" value={this.state.quantity} onChange={this._handleChange}/><br/>
+      <label>Price:</label><input type="number" value={this.state.price} readOnly="true" /><br/>
+      <label>Status:</label><input type="text" value={this.state.status} readOnly="true" /><br/>
+      <label>Created on:</label><input type="date" value={this.state.current_date} onChange={this._handleChange}/><br/>
+      <button type="submit">Save</button>
       </form>
       </div>
     )
