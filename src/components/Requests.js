29f2b2f74288499eaf17ff862c07c1bc8931feae
kuljitkaur27 <kuljitkaur1984@gmail.com>
2020-04-11 00:00:36 +1000

    Edit Request

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 import axios from 'axios';
+import EditRequest from './EditRequest';
+import './Requests.css';
+
 
 const SERVER_URL_requests = 'http://localhost:3000/requests';
 
@@ -10,7 +14,8 @@ class Requests extends Component {
     super();
     this.state = {
       reqs: [],
-      id : 0
+      id : 0,
+      show: false
     };
 
     console.log("array:",this.state.reqs);
@@ -43,12 +48,26 @@ class Requests extends Component {
           )
   }
 
+  showModal = () => {
+    this.setState({ show: true });
+  };
 
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+// .............................
+  getData = (editData) => {
+// This is the row data from ChildComponent
+  console.log(editData);
+}
+
+//...................................
   render(){
     return(
       <div>
         <h3>All Requests</h3>
         <table>
+        <tbody>
               <tr>
                 <th>Branch ID</th>
                 <th>Product ID</th>
@@ -65,23 +84,38 @@ class Requests extends Component {
                   <td>{req.price}</td>
                   <td>{req.status}</td>
                   <td>
-                    <button type="button"><Link to={{
-                                            pathname: "/editRequest",
-                                            data: req.id
-                                          }}>Edit</Link>
-                    </button>
-                    &nbsp;
+                      <Modal show={this.state.show} handleClose={this.hideModal}>
+                        <EditRequest editData={req} onChange={this.getData} />
+                      </Modal>
+                      <button type="button" onClick={this.showModal}>
+                      Edit
+                      </button>
+                  </td>
+                  <td>
                     <button type="button" onClick={() => this._deleteRequest(this,req.id)}>Delete</button>
                   </td>
                 </tr>
               ))}
-
+              </tbody>
           </table>
-
       </div>
     )
   }
 
 }
+
+const Modal = ({ show, handleClose,  children }) => {
+
+  const showHideClassName = show == true ? "modal display-block" : "modal display-none";
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        {children}
+        <button onClick={handleClose}>close</button>
+      </section>
+    </div>
+  );
+};
 
 export default Requests;
