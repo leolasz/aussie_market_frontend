a3afae0ae04986355575f783033933fe001f7794
kuljitkaur27 <kuljitkaur1984@gmail.com>
2020-04-12 20:39:13 +1000

    All Products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,75 +1,179 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import { Col, Button, Table} from "react-bootstrap";
 
 
-const SERVER_URL_products = 'http://localhost:3000/products.json';
-    
+const SERVER_URL_products = 'http://localhost:3000/products';
+
 class Products extends Component {
 
   constructor(){
     super();
     this.state = {
-      prod: []
+      productList: []
     };
-
-    console.log("array:",this.state.prod);
 
     const fetchresult = () => {
         axios.get(SERVER_URL_products).then((results) => {
           console.log(results.data);
-          this.setState({prod: results.data});
+          this.setState({productList: results.data});
         })
     }
 
     fetchresult();
 
-        this.deleteProd = this.deleteProd.bind(this);
+        this.deleteRecord = this.deleteRecord.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.onSave = this.onSave.bind(this);
 
   }
 
-  deleteProd = (index) => {
-      const prod = Object.assign([],this.state.prod);
-      prod.splice(index,1);
-      this.setState({prod:prod});
-    }
+  handleChange(data, e) {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  onSave() {
+    axios.get(SERVER_URL_products)
+      .then(response => {
+          this.setState({ productList: response.data});
+        })
+      //.catch(err => { console.log('Something bad is happened:', err) });
+  }
+
+  deleteRecord(i) {
+    let data = this.state.productList[i];
+    var id = data.id;
+
+    alert("are you sure you want to Delete this item ?");
+    this.setState({
+      productList: this.state.productList.filter((item, index) => {
+        return index !== i;
+      }),
+    });
+
+    axios
+      .delete(`${SERVER_URL_products}/${id}`)
+      .then((result) => {
+
+      });
+  }
+
+  editRecord(i) {
+    let request = this.state.productList[i];
+    this.refs.editForm.setState({
+          id: request.id,
+          item: request.item,
+          price: request.price,
+          image: request.image
+    });
+  }
 
   render(){
     return(
       <div>
         <h3>All Products</h3>
-        <table>
+          <Table striped bordered hover>
+            <thead>
               <tr>
                 <th>Item</th>
                 <th>Price</th>
-                <th>Quantity</th>
-                
-                
+                <th>Image</th>
               </tr>
-
-              {this.state.prod.map(prod => (
-                <tr key={prod.id}>
-                  <td>{prod.item}</td>
-                  <td>{prod.price}</td>
-                  <td>{prod.quantity}</td>
-                 
-                
-                  
-                 
+            </thead>
+            <tbody>
+              {this.state.productList.map((item, i) => (
+                <tr key={i}>
+                  <td>{item.item}</td>
+                  <td>{item.price}</td>
+                  <td>{item.image}</td>
                   <td>
-                    <button type="button">Edit</button>
-                    &nbsp;<button type="button" onClick={this.deleteProd.bind(prod.id)}>Delete</button>
+                  <Button variant="info" onClick={() => this.editRecord(i)}>
+                    Edit
+                  </Button>
+                  </td>
+                  <td>
+                    <Button variant="danger" onClick={() => this.deleteRecord(i)}>
+                      Delete
+                    </Button>
                   </td>
                 </tr>
               ))}
-
-          </table>
-          <button><Link to={'/main'} >Back </Link></button>
+            </tbody>
+          </Table>
+          <Edit ref='editForm' save={this.onSave}/>
       </div>
-      
     )
   }
+}
 
+class Edit extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id : '',
+      item: '',
+      price: '',
+      image: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name] : event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    var id = this.state.id;
+    axios.post(`${SERVER_URL_products}/${id}/update`, this.state)
+      .then(
+        (result) => {
+          this.props.save();
+          this.setState({id : '',
+                        item: '',
+                        price: '',
+                        image: ''});
+        }
+      )
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Product:
+          <input
+            type="string"
+            name='item'
+            value={this.state.item}
+            onChange={this.handleChange} />
+        </label>
+        <label>
+          Price:
+          <input
+            type="float"
+            name='price'
+            value={this.state.price}
+            onChange={this.handleChange} />
+        </label>
+        <label>
+          Image:
+          <input
+            type="text"
+            name='image'
+            value={this.state.image}
+            onChange={this.handleChange} />
+        </label>
+        <input type="hidden" name='id' value={this.state.id}/>
+        <button type="submit">Save</button>
+        <Button variant="danger"><Link to={'/main'}>Back Home page </Link></Button>{' '}
+      </form>
+    );
+  }
 }
 
 export default Products;
